c9cd494224f4fcbf3c7136c37f23058f8c76e824
Abhishek Chakrabartti <abhi@Abhisheks-MacBook-Pro.local>
2019-08-18 01:43:58 +0530

    save to dictionary functionality

--- worditionary/src/main/java/com/happiestMinds/Worditionary/worditionary/services/SaveWordService.java
+++ worditionary/src/main/java/com/happiestMinds/Worditionary/worditionary/services/SaveWordService.java
@@ -2,13 +2,22 @@ package com.happiestMinds.Worditionary.worditionary.services;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
+import org.springframework.boot.json.JsonParser;
+import org.springframework.boot.json.JsonParserFactory;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class SaveWordService {
@@ -23,6 +32,18 @@ public class SaveWordService {
             if(strValue.matches("<.*>"))
             	wordsList.add(strValue);
 		}
+		
+		File f = ResourceUtils.getFile("dictionary.json");
+		String content = new String(Files.readAllBytes(f.toPath()));
+		JsonParser parser = JsonParserFactory.getJsonParser();
+		Map<String, Object> map = parser.parseMap(content);
+		for(String st:wordsList) {
+			if(!map.containsKey(st))
+				map.put(st, new Integer(0));
+		}
+		FileWriter filew = new FileWriter(f);
+		ObjectMapper mapper = new ObjectMapper();
+		filew.write(mapper.writeValueAsString(map));
 		return "";
 	}
 }
